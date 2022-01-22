07e885072a22095fba21273883f37091fbf6386d
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2022-01-22 18:42:09 +0100

    Refactor

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='quantutils',
+   version='1.0',
+   description='Quantitative Finance utilities',
+   author='Piotr Mikler',
+   author_email='piotr.mikler1997@gmail.com',
+   packages=['quantutils'],  #same as name
+   install_requires=[] #external packages as dependencies
+)
