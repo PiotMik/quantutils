5ab61a30f25dbc38e96e0ffecbde8216cd194b3b
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2022-02-22 00:24:41 +0100

    Add docstrings

--- quantutils/simulations/simulation_schemes.py
+++ quantutils/simulations/simulation_schemes.py
@@ -3,7 +3,30 @@ from typing import Callable, Optional
 
 
 def euler_maruyama(x0: float, a_t: Callable, b_t: Callable, n_steps: int, dt: float, random_toss: Optional[np.array] = None):
+        """
+        Simulate a stochastic process according to Euler-Maruyama method.
 
+        Parameters
+        ----------
+        x0: float
+            Starting value for the process
+        a_t: Callable f: (t, x) -> R
+            First characteristic of a stochastic process,
+        b_t: Callable f: (t, x) -> R
+            Second characteristic of a stochastic process
+        n_steps: int
+            Number of time discretization steps
+        dt: float
+            Lenght of a time step
+        random_toss: Optional[np.array]
+            Array of +1/-1, which is used for Wiener Process approximation.
+            If not provided, it gets generated automatically.
+
+        Returns
+        -------
+        (np.array, np.array)
+            Time array and simulated stochastic process values
+        """
         if not random_toss:
             random_toss = np.random.rand(n_steps).round()*2 - 1
         if random_toss.shape != (n_steps, ):
